8742455a88bf4a9d0027fc48e096e6104fe18ce5
TheoKm <longxinmoyue@gmail.com>
2018-04-21 21:34:13 +0800

    solved chiese Character garbled

--- html_outputer.py
+++ html_outputer.py
@@ -10,6 +10,7 @@ class HtmlOutputer(object):
     def output_html(self):
         fout = open('output.html','w',encoding='utf-8')
         fout.write("<html>")
+        fout.write("<head><meta http-equiv='content-type' content='text/html;charset=utf-8'></head>")
         fout.write("<body>")
         fout.write("<table>")
         # ascii
